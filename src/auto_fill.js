34f1ca31f26c6ee7dbb73d719a73c999490f55f9
valevo <valevogelmann@gmail.com>
2026-06-16 08:46:42 +0200

    Update auto_fill.js

--- src/auto_fill.js
+++ src/auto_fill.js
@@ -1,3 +1,26 @@
 function autofill() {
-  
+var level = document.getElementById("level");
+var lang = document.getElementById("language");
+var title = document.getElementById("title");
+
+var curRAs = RAs[lang][level]
+
+if (!(title in curRAs)) {
+return;
+}
+
+var curRA = curRAs[title];
+
+document.getElementById("abstract").innerHTML = curRA.abstract;
+document.getElementById("main-text").innerHTML = curRA["Main-text"];
+
+for (var relRA in curRA["RelatedAides"]) {
+
+}
+}
+
+
+function assembleRA() {
+
+
 }
